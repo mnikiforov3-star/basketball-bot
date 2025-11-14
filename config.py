8a34834c954eb6c8f938bb36db27efc9bd75c454
mnikiforov3-star <m.nikiforov3@gmail.com>
2025-11-14 11:30:48 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+BOT_TOKEN = "123456:ABC..."
+CHAT_ID   = -1001234567890  # свой ID или группы
+
+# Flashscore не требует ключа, но можно использовать прокси/headers
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+}
+
+# Дополнительные API (опционально)
+ODDS_API_KEY = "ваш_ключ_oddsapi"  # https://the-odds-api.com/
+NEWS_API_KEY = "ваш_ключ_newsapi"  # https://newsapi.org/
